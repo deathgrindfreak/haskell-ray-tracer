5d8b423692f82e92f94218ed78c9c2a8afa9982e
Cooper Bell <bell.cooper@heb.com>
2023-02-20 14:46:05 -0600

    Convert lighting tests to hedgehog/tasty

--- test/Test/Light.hs
+++ test/Test/Light.hs
@@ -1,47 +1,53 @@
-module Test.Light (spec_Light) where
+module Test.Light (test_Light) where
 
 import RayTracer.Color
 import RayTracer.Light hiding (position)
 import RayTracer.Tuple
 import Test.Helper.Approximate
-import Test.Hspec
 
-spec_Light :: Spec
-spec_Light = describe "Light" $ do
+import qualified Hedgehog as HH
+import qualified Test.Tasty as Tasty
+import qualified Test.Tasty.Hedgehog as THH
+
+test_Light :: Tasty.TestTree
+test_Light =
   let m = defaultMaterial
       position = Point 0 0 0
-
-  it "Lighting with the eye between the light and the surface" $ do
-    let eyev = Vec 0 0 (-1)
-        normalv = Vec 0 0 (-1)
-        light = PointLight (Point 0 0 (-10)) (Color 1 1 1)
-        result = lighting m light position eyev normalv
-    result `shouldApproximate` Color 1.9 1.9 1.9
-
-  it "Lighting with the eye between the light and the surface, eye offset 45°" $ do
-    let eyev = Vec 0 (sqrt 2 / 2) (-sqrt 2 / 2)
-        normalv = Vec 0 0 (-1)
-        light = PointLight (Point 0 0 (-10)) (Color 1 1 1)
-        result = lighting m light position eyev normalv
-    result `shouldApproximate` Color 1.0 1.0 1.0
-
-  it "Lighting with the opposite the surface, light offset 45°" $ do
-    let eyev = Vec 0 0 (-1)
-        normalv = Vec 0 0 (-1)
-        light = PointLight (Point 0 10 (-10)) (Color 1 1 1)
-        result = lighting m light position eyev normalv
-    result `shouldApproximate` Color 0.7364 0.7364 0.7364
-
-  it "Lighting with the eye in the path of the reflection vector" $ do
-    let eyev = Vec 0 (-sqrt 2 / 2) (-sqrt 2 / 2)
-        normalv = Vec 0 0 (-1)
-        light = PointLight (Point 0 10 (-10)) (Color 1 1 1)
-        result = lighting m light position eyev normalv
-    result `shouldApproximate` Color 1.6364 1.6364 1.6364
-
-  it "Lighting with the light behind the surface" $ do
-    let eyev = Vec 0 0 (-1)
-        normalv = Vec 0 0 (-1)
-        light = PointLight (Point 0 0 10) (Color 1 1 1)
-        result = lighting m light position eyev normalv
-    result `shouldApproximate` Color 0.1 0.1 0.1
+   in Tasty.testGroup
+        "Light"
+        [ THH.testProperty "Lighting with the eye between the light and the surface" $
+            HH.property $ do
+              let eyev = Vec 0 0 (-1)
+                  normalv = Vec 0 0 (-1)
+                  light = PointLight (Point 0 0 (-10)) (Color 1 1 1)
+                  result = lighting m light position eyev normalv
+              result ~== Color 1.9 1.9 1.9
+        , THH.testProperty "Lighting with the eye between the light and the surface, eye offset 45°" $
+            HH.property $ do
+              let eyev = Vec 0 (sqrt 2 / 2) (-sqrt 2 / 2)
+                  normalv = Vec 0 0 (-1)
+                  light = PointLight (Point 0 0 (-10)) (Color 1 1 1)
+                  result = lighting m light position eyev normalv
+              result ~== Color 1.0 1.0 1.0
+        , THH.testProperty "Lighting with the opposite the surface, light offset 45°" $
+            HH.property $ do
+              let eyev = Vec 0 0 (-1)
+                  normalv = Vec 0 0 (-1)
+                  light = PointLight (Point 0 10 (-10)) (Color 1 1 1)
+                  result = lighting m light position eyev normalv
+              result ~== Color 0.7364 0.7364 0.7364
+        , THH.testProperty "Lighting with the eye in the path of the reflection vector" $
+            HH.property $ do
+              let eyev = Vec 0 (-sqrt 2 / 2) (-sqrt 2 / 2)
+                  normalv = Vec 0 0 (-1)
+                  light = PointLight (Point 0 10 (-10)) (Color 1 1 1)
+                  result = lighting m light position eyev normalv
+              result ~== Color 1.6364 1.6364 1.6364
+        , THH.testProperty "Lighting with the light behind the surface" $
+            HH.property $ do
+              let eyev = Vec 0 0 (-1)
+                  normalv = Vec 0 0 (-1)
+                  light = PointLight (Point 0 0 10) (Color 1 1 1)
+                  result = lighting m light position eyev normalv
+              result ~== Color 0.1 0.1 0.1
+        ]
